24cdc4cab818a3d0e5e6f05877aab5e611061db8
upendra <upendrarajput9911@gmail.com>
2022-02-13 12:36:11 +0530

    done

--- index.js
+++
@@ -1,7 +0,0 @@
-const { difference } = require("lodash");
-let lodash= require("lodash")
-console.log(moment().format('MMMM Do YYYY, h:mm:ss a')); 
-console.log(moment().format('dddd'));                    // Saturday
-console.log(moment().format("MMM Do YY"));  
-
-
